a9ec16833a7496abc9fd0eab0b20c7580c75afd4
Pietro Lanza <pietrolp333@gmail.com>
2023-12-20 14:44:32 -0300

    fix: front auth fixed

--- frontend/src/pages/private/Dashboard/index.jsx
+++ frontend/src/pages/private/Dashboard/index.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { Navigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 export default function DashBoard() {
     const [name, setName] = useState('');
     const [isLogged, setIsLogged] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleLogout = async () => {
         await fetch('http://localhost:8000/api/logout', {
@@ -15,6 +17,7 @@ export default function DashBoard() {
     }
 
     useEffect(() => {
+        setIsLoading(true);
         (
             async () => {
                 try {
@@ -31,13 +34,21 @@ export default function DashBoard() {
                 }
             }
         ) ();
+        setIsLoading(false);
     })
 
     if(isLogged) {
         return (
             <>
-                <h1>hi {name}</h1>
-                <button onClick={handleLogout}>Logout</button>
+                {isLoading && (
+                    <ClipLoader color="black" />
+                )}
+                {!isLoading && (
+                    <>
+                        <h1>hi {name}</h1>
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                )}
             </>
         )
     } else {
