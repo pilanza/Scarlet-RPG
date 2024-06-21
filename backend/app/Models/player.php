d5c101faea28f9d07b16e01738f996b5e8f3981e
Pietro Lanza <pietrolp333@gmail.com>
2024-06-20 23:19:49 -0300

    :sparkles: feat: player model done

--- backend/app/Models/player.php
+++ backend/app/Models/player.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class player extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'bank_coins',
+        'coins',
+        'level',
+        'xp',
+        'role',
+        'area',
+        'loyalty_rank',
+        'weapon',
+        'armor',
+        'blessing',
+    ];
 }
